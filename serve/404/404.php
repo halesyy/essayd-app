7e25557529444bc387ace07a86e1d4928f331c4f
KastelOfficial <markkelles@gmail.com>
2017-10-22 12:41:59 -0400

    added 404 page
    I knowwww it doesn't fit the site, and you will 99% reject it but I kept
    it anyways because its cute :^)

---
+++ serve/404/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="../../public/css/404.css">
+    <meta charset="utf-8">
+    <title>Page not Found</title>
+  </head>
+  <body>
+
+  <div id="uhoh">
+    <h1>Uh oh, you seem lost! Get back to where you belong!</h1>
+    <h2>You might have been trying to reach one of our popular destinations.</h2>
+  </div id="uhoh">
+  <div id="topnavbar">
+    <ul>
+      <li><a href="https://essaydapp.com">Home</a></li>
+      <li><a href="https://essaydapp.com/editor">Editor</a></li>
+      <li><a href="https://essaydapp.com/dashboard">Dashboard</a></li>
+    </ul>
+  </div id="topnavbar">
+  </body>
+</html>
