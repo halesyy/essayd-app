e9f02d355cb837c2a6d86ddeaafeb20abd8ca50a
Unknown <markkelles@gmail.com>
2017-11-01 21:43:55 -0400

    Added an about page
    its not that great, so make improvements as you see fit but I haven't coded in a week and was dying so I had to get some practice and yea this happened kthxbai

---
+++ serve/aboutus/aboutus.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Essay'd | About us</title>
+    <link rel="stylesheet" href="../../public/css/modules/aboutus.css">
+  </head>
+  <body>
+
+    <div id="navbar">
+      <ul>
+        <li><a href="https://essaydapp.com">Home</a></li>
+        <li><a href="https://essaydapp.com/editor">Editor</a></li>
+        <li><a href="https://essaydapp.com/dashboard">Dashboard</a></li>
+      </ul>
+    </div id="navbar">
+
+  <div id="intro">
+    <h1>Who are you?</h1>
+    <p>Hey, we're a couple of high school students working to provide this website for students around the world facing essay-writing problems!</p>
+  </div id="intro">
+
+  <div id="jekbio">
+    <img src="../../public/images/jek.jpg" height="200px" width="200px" alt="Jekpp">
+    <p>I'm Jek, a full-stack developer and the main developer on this website. I'm also the only useful one. Wanna talk? Get in contact with me on Discord at Jek잨#3847 (But I probably will never reply on Discord) or tweet me <a href="https://twitter.com/jeklington">@Jeklington</a>.</p>
+  </div id="jekbio">
+
+  <div id="kastelbio">
+    <img src="../../public/images/kastel.png" height="200px" width="200px" alt="Kastelpp">
+    <p>Hey, I'm Kastel! I am currently a student studying front-end web development, and I made this page. I think it's pretty great, tell me what you think on Discord at Kastel#8335 or tweet me <a href="https://twitter.com/kastelofficial">@Kastelofficial</a>!</p>
+  </div id="kastelbio">
+
+  </body>
+</html>
